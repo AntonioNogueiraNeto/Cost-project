738bac654ac3aba6a40b8fa116bdf0c67e0acded
Antônio <antonio.nogueiraneto@icloud.com>
2022-09-28 13:05:01 -0300

    componentização do form, criado a pasta form com os components e styles

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,21 +1,27 @@
-function ProjectForm() {
-  return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto" />
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento total" />
-      </div>
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
 
-      <div>
-        <select name="category_id">
-          <option disabled>Selecione a categoria</option>
-        </select>
-      </div>
-      <div>
-        <input type="submit" value="Criar Projeto" />
-      </div>
+function ProjectForm({btnText}) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        name="name"
+        text="Nome do Projeto"
+        placeholder="Insira o nome do projeto"
+      />
+
+      <Input
+        type="number"
+        name="budget"
+        text="Orçamento do Projeto"
+        placeholder="Insira o orçamento total"
+      />
+
+      <Select name="category_id" text="Selecione a categoria"/>
+      <SubmitButton text={btnText} />
     </form>
   );
 }
