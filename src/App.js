b898d79cf46d85d3a81c33611006b361fef992aa
Antônio <antonio.nogueiraneto@icloud.com>
2022-09-27 23:22:21 -0300

    adicionando os links e rotas, criado a pasta components e pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/pages/Home"
+import Contact from "./components/pages/Contact"
+import Company from "./components/pages/Company"
+import NewProject from "./components/pages/NewProject"
+
 function App() {
   return (
-    <h1>Oi</h1>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/company" element={<Company />} />
+      <Route path="/newProject" element={<NewProject />} />
+    </Routes>
+    <p>Footer</p>
+    </Router>
   );
 }
 
